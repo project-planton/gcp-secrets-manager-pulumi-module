f6557753f020a7076c080b358612e6500678aeb3
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-07 18:43:31 +0530

    more renames

--- pkg/outputs/outputs.go
+++ pkg/outputs/outputs.go
@@ -7,13 +7,13 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/auto"
 )
 
-func PulumiOutputsToStackOutputsConverter(stackOutput auto.OutputMap,
+func PulumiOutputsToStackOutputsConverter(pulumiOutputMap auto.OutputMap,
 	input *gcpsecretsmanager.GcpSecretsManagerStackInput) *gcpsecretsmanager.GcpSecretsManagerStackOutputs {
 	stackOutputs := &gcpsecretsmanager.GcpSecretsManagerStackOutputs{
 		SecretIdMap: make(map[string]string),
 	}
 	for _, secretName := range input.ApiResource.Spec.SecretNames {
-		stackOutputs.SecretIdMap[secretName] = autoapistackoutput.GetVal(stackOutput,
+		stackOutputs.SecretIdMap[secretName] = autoapistackoutput.GetVal(pulumiOutputMap,
 			fmt.Sprintf("%s-id", secretName))
 	}
 	return stackOutputs
